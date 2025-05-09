715f7af9eaa7fedb2d289d240aed4edad2435f11
mohammad ziadeh <mohammedziadeh6@gmail.com>
2025-05-09 21:29:32 +0300

    login route

--- src/layout/publicSide/navbar.jsx
+++ src/layout/publicSide/navbar.jsx
@@ -3,8 +3,10 @@ import "./styles/navbar.css";
 export default function navbar() {
   return (
     <nav className="navBar">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <div>
+      <a  href="/">Home</a>
+      <a style={{marginLeft:"120px"}} href="/about">About</a>
+      </div>
       <a href="http://localhost:8000/login">Log in</a>
     </nav>
   );
